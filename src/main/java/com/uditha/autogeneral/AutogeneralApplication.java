faf4e631ff0dd616e76bcd7cb1b35442bd92178e
Uditha Karunaratne <udithaindika@gmail.com>
2018-10-07 14:29:58 +1100

    Initial Project setup

---
+++ src/main/java/com/uditha/autogeneral/AutogeneralApplication.java
@@ -0,0 +1,12 @@
+package com.uditha.autogeneral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutogeneralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutogeneralApplication.class, args);
+	}
+}
---
+++ src/test/java/com/uditha/autogeneral/AutogeneralApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uditha.autogeneral;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutogeneralApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
